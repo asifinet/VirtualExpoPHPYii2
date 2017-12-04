45f6d4b8f1fc2ab82604c065741205350c31a211
Muhammad Asif <Muhammad Asif>
2017-12-04 19:57:25 +0500

    VirtualExpo_v1
    Apachee,Mysql,PHP,yii2 framework

---
+++ frontend/views/site/expo_comp_service.php
@@ -0,0 +1,36 @@
+
+<?php require_once('I:/wamp64/www/advanced/common/config/Mysql.php'); 
+
+       mysql_select_db($database_mysql, $mysql);
+        $OwnerId = $_GET['id'];
+
+	if ($_GET['action'] == 'listpoints') {
+		$query = "SELECT g.id,g.company_rep_name,g.office_phone,g.primary_email,g.file
+                    FROM expo_db.expo_company g
+                    WHERE g.id = $OwnerId
+	             ORDER BY g.id";
+    
+		   $result = mysql_query($query, $mysql) or die(mysql_error());
+           $points = array();
+		
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+             array_push($points, array('id' => $row['id'],
+			                           'name' => $row['company_rep_name'],
+			                            'phone' => $row['office_phone'],
+									    'email' => $row['primary_email'],
+										'mkt_dir' => $row['file	']));
+		
+		   
+		}
+		
+		echo json_encode(array("company" => $points));
+		exit;
+	}
+function success($data) { 
+  die(json_encode(array('status' =>'success', 'data' =>$data))); 
+}
+	function fail($message) {
+		die(json_encode(array('status' => 'fail', 'message' => $message)));
+	}
+	?>
+    
